3e9331184b319fcafbed0b4110f1fbf13bdbdfff
ramyashankaralingam <ramya.shankaralingam@service.nsw.gov.au>
2023-08-30 11:19:58 +1000

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,41 @@
+import Title from "./Title";
+import { tours } from "../data";
+const Tours = () => {
+  return (
+    <section class="section" id="tours">
+      <Title title="Featured" subTitle="tours" />
+
+      <div class="section-center featured-center">
+        {tours.map((tour) => {
+          const { id, image, date, title, text, location, duration, cost } =
+            tour;
+          return (
+            <article class="tour-card" key={id}>
+              <div class="tour-img-container">
+                <img src={image} class="tour-img" alt={title} />
+                <p class="tour-date">august 26th, 2020</p>
+              </div>
+              <div class="tour-info">
+                <div class="tour-title">
+                  <h4>{title}</h4>
+                </div>
+                <p>{text}</p>
+                <div class="tour-footer">
+                  <p>
+                    <span>
+                      <i class="fas fa-map"></i>
+                    </span>{" "}
+                    {location}
+                  </p>
+                  <p>{duration} days</p>
+                  <p>from ${cost}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Tours;
